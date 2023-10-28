758ceb86ab1cf995f89ef93941f1660c1709902c
Kael <kaelwd@gmail.com>
2023-10-28 19:07:30 +1100

    fix: sandbox same-origin requests in iframe

--- src/output/Preview.vue
+++ src/output/Preview.vue
@@ -82,7 +82,6 @@ function createSandbox() {
       'allow-modals',
       'allow-pointer-lock',
       'allow-popups',
-      'allow-same-origin',
       'allow-scripts',
       'allow-top-navigation-by-user-activation',
     ].join(' ')
